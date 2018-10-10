bcc826803d31b475b653959a7bc4ce406cbd5297
imshenzhuo <im.shenzhuo@gmail.com>
2018-10-10 10:02:44 +0800

    update acc

--- calcACC.py
+++ calcACC.py
@@ -1,13 +1,19 @@
 import numpy as np
-
+from sklearn import metrics
 from hungarian import Hungarian
+
+
+def NMI(labels, labels_pred):
+    nmi = metrics.normalized_mutual_info_score(labels, labels_pred)
+    return nmi
+
 
 def calcACC(groundTruth, predValue):
     """
     计算使用预测值与真实值的最佳匹配的正确率
     :param groundTruth:真实值 np.ndarray
     :param predValue:预测值 np.ndarray
-    :return:ACC:正确率 
+    :return:ACC:正确率
     """
     if len(groundTruth.shape) != 1:
         groundTruth = groundTruth.reshape(groundTruth.shape[0])
@@ -22,14 +28,28 @@ def calcACC(groundTruth, predValue):
 def bestMap(L1, L2):
     """
     两个向量的最佳匹配
-    :param L1:np.ndarray
-    :param L2:np.ndarray
+    :param L1:np.ndarray 真实值
+    :param L2:np.ndarray 预测标签
     :return:L1对L2的最佳匹配 np.ndarray
     """
     Label1 = np.unique(L1)
     nClass1 = len(Label1)
     Label2 = np.unique(L2)
     nClass2 = len(Label2)
+    
+    # predValue中间有跳过的label ： 0，1，5，6，10
+    if nClass2 < np.max(L2):
+        newLabel2 = np.argsort(Label2)
+        for i in range(nClass2):
+            if Label2[i] != newLabel2[i]:
+                L2 = np.where(L2 == Label2[i], newLabel2[i], L2)
+        Label2 = newLabel2
+
+    # 如果groundTruth是从1开始计数，而predValue是从0开始计数，那么predValue += 1
+    if nClass1 == np.max(L1) and nClass2 > np.max(L2):
+        L2 = L2 + 1
+        Label2 = Label2 + 1
+
     nClass = max(nClass1, nClass2)
     G = np.zeros((nClass, nClass))
     for i in range(nClass1):
